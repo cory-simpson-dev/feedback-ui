5b35574d4e6a45ce54985fc5056fa72e8081325e
Cory Simpson <cory.simpson.dev@gmail.com>
2022-08-30 12:01:55 -0700

    initial build

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import FeedbackData from "./data/FeedbackData"
-
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+// curly braces because it is not default export in FeedbackContext.js
+import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
-    const[feedback, setFeedback] = useState(FeedbackData)
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-    }
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
+        
     return (
-        <>
+        <FeedbackProvider>
+            <Router>
             <Header />
             <div className='container'>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm />
+                            <FeedbackStats />
+                            <FeedbackList />
+                            <AboutIconLink />
+                        </>
+                    }>    
+                    </Route>
+
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
             </div>
-        </>
-        
+        </Router>
+        </FeedbackProvider>       
     )
 }
 
